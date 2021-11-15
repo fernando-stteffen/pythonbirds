4616551d2fc55a3120b37f690f3b307b9c055639
ftrunks <frodovalsegundo@gmail.com>
2021-11-15 10:29:35 -0300

    Criado atributos nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,17 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=32):
+        self.nome = nome
+        self.idade = idade
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Fernando')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Stteffen'
+    print(p.nome)
+    print(p.idade)
